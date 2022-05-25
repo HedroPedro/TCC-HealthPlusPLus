17cc737cdca4585da4f6a5dfc504f15c229325bb
HedroPedro <emailgenericomeu@gmail.com>
2022-05-25 15:28:41 -0300

    existeUsuario finalizado

--- HealthPlusPlus/src/modelador/JDBCUsuario.java
+++ HealthPlusPlus/src/modelador/JDBCUsuario.java
@@ -11,17 +11,15 @@ public class JDBCUsuario {
         this.c = c;
     }
     
-    public boolean existeUsuario(String nome, char[] senha){ //Checa se existe usuario
-        System.out.println(nome + " " + String.copyValueOf(senha));
+    public boolean existeUsuario(String nome, String senha){ //Checa se existe usuario
         String sql = "SELECT COD_USUARIO from tb_usuario WHERE NOME = ? and SENHA = ?";
         PreparedStatement ps;
         try {
             ps = this.c.prepareStatement(sql);
             ps.setString(1, nome);
-            ps.setString(2, String.copyValueOf(senha));
+            ps.setString(2, senha);
             ResultSet rs = ps.executeQuery();
             while(rs.next()){
-                System.out.print(rs.getInt(1));
                 return true;
             }
         } catch (Exception ex) {
