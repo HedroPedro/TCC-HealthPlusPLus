182699c7b6928acdff2d585a784f5c2e8bee2a47
HedroPedro <emailgenericomeu@gmail.com>
2022-06-01 14:19:00 -0300

    Adicionado listarAgendamentos

--- HealthPlusPlus/src/modelos/Agendamento.java
+++ HealthPlusPlus/src/modelos/Agendamento.java
@@ -5,14 +5,16 @@ import java.sql.Date;
 public class Agendamento {
     private int codigo;
     private Date datahora;
+    private int cod_usuario;
     private float preco;
     private int cod_cliente;
     
     public Agendamento(){}
 
-    public Agendamento(int codigo, Date datahora, float preco, int cod_cliente) {
+    public Agendamento(int codigo, Date datahora, int cod_usuario, float preco, int cod_cliente) {
         this.codigo = codigo;
         this.datahora = datahora;
+        this.cod_usuario = cod_usuario;
         this.preco = preco;
         this.cod_cliente = cod_cliente;
     }
