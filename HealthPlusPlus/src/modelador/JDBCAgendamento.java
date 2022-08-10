046b4ea62178e9858a31b394d21beb2fa5e670b2
HedroPedro <emailgenericomeu@gmail.com>
2022-08-10 08:10:44 -0300

    Mais alteração

--- HealthPlusPlus/src/modelador/JDBCAgendamento.java
+++ HealthPlusPlus/src/modelador/JDBCAgendamento.java
@@ -40,15 +40,14 @@ public class JDBCAgendamento {
         return agendamentos;
     }
     
-    public void inserirAgendamento(Agendamento a){
-        String sql = "Insert into Tb_Agendamento(DATAHORA, PRECO, COD_CLIENTE) values(?,?,?)";
+    public void inserirAgendamento(Agendamento a, String nome){
+        String sql = "Insert into Tb_Agendamento(DATAHORA, PRECO) values(?,?)";
         try {
             PreparedStatement ps = c.prepareStatement(sql);
             ps.setDate(1,Date.valueOf(a.getDatahora().toString()));
             ps.setFloat(2, a.getPreco());
             ps.setInt(3, a.getCod_cliente());
             ps.execute();
-            //ps.setDate(0, a.getDatahora());
         } catch (SQLException ex) {
             Logger.getLogger(JDBCAgendamento.class.getName()).log(Level.SEVERE, null, ex);
         }
